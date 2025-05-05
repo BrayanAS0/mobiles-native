ce8ae6c6c6b92232ecd9c929e3ce670d4adef857
BrayanAS0 <el.el.brayan16@gmail.com>
2025-05-04 22:12:34 -0600

    working with nativewind

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -17,7 +17,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText className=' bg-red-500' type="title">Welcome!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
