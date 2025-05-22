12bc221a5a04b504cce02221af66ed8c618c8a9b
BrayanAS0 <el.el.brayan16@gmail.com>
2025-05-21 19:16:04 -0600

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ export default function Index() {
     const router = useRouter();
 
     useEffect(() => {
-        router.replace("/Login"); // Redirige siempre a Login al abrir la app
+        router.replace("/login"); // Redirige siempre a Login al abrir la app
     }, []);
 
     return null;
